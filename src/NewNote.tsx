5a763a86c84ed5b4e9793f937f8bb6b5af6016d6
Mauricio Robayo <rfmajo@gmail.com>
2020-07-13 12:35:31 -0500

    chore: make input a controlled component

--- src/NewNote.tsx
+++ src/NewNote.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default () => {
+  const [ note, setNote ] = useState('')
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNote(event.target.value)
+  }
   return (
     <>
-      <input type="text" name='note' placeholder='note' />
+      <input type="text" name='note' placeholder='note' value={note} onChange={onChange} />
       <button>Add note</button>
     </>
   )
